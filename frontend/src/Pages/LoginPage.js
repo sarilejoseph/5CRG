2db340f21cdcc422d12a547291ebc73e07e561a0
sarilejoseph <josephjr.sarile@hcdc.edu.ph>
2025-05-19 10:38:01 +0800

    19/05/25

--- frontend/src/Pages/LoginPage.js
+++ frontend/src/Pages/LoginPage.js
@@ -125,33 +125,25 @@ const LoginPage = () => {
                       <svg
                         className="w-5 h-5 text-gray-500"
                         fill="currentColor"
-                        viewBox="0 0 20 20"
+                        viewBox="0 0 24 24"
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         {showPassword ? (
-                          <path d="M10 12a2 2 0 100-4 2 2 0 000 4z"></path>
+                          <path
+                            fillRule="evenodd"
+                            d="M12 4.5c-5 0-9 5.5-9 5.5s4 5.5 9 5.5 9-5.5 9-5.5-4-5.5-9-5.5zm0 2c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"
+                            clipRule="evenodd"
+                          />
                         ) : (
-                          <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 00-2.79.588l.77.771A5.944 5.944 0 018 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0114.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"></path>
+                          <path
+                            fillRule="evenodd"
+                            d="M12 4.5c-5 0-9 5.5-9 5.5s4 5.5 9 5.5 9-5.5 9-5.5-4-5.5-9-5.5zm0 2c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM2.83 2.83l18.34 18.34-1.42 1.42L1.41 4.25 2.83 2.83z"
+                            clipRule="evenodd"
+                          />
                         )}
                       </svg>
                     </div>
                   )}
-                </div>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                    id="remember_me"
-                    type="checkbox"
-                    className="h-4 w-4 text-blue-600 border-gray-300 rounded"
-                  />
-                  <label
-                    htmlFor="remember_me"
-                    className="ml-2 block text-sm text-gray-600"
-                  >
-                    Remember Username
-                  </label>
                 </div>
               </div>
 
@@ -161,16 +153,6 @@ const LoginPage = () => {
               >
                 Sign In
               </button>
-              {/* 
-              <div className="text-sm text-center mt-4">
-                <span className="text-gray-600">Don't have an account? </span>
-                <Link
-                  to="/registerpage"
-                  className="text-blue-600 hover:text-blue-800"
-                >
-                  Sign up
-                </Link>
-              </div> */}
             </form>
             {errorMessage && (
               <div className="mt-4 p-2 bg-red-100 text-red-700 rounded-md">
